78b3a69f1e40c66c990f4a46b9d00ee6de1f2b8c
s4llm4n <55424625+s4llm4n@users.noreply.github.com>
2024-04-05 13:41:27 +0700

    Update NavbarDS.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
   return (
     <div className={color ? 'header header-bg' : 'header'}>
       <Link to="/">
-        <h1></h1>
+        <h1>GLX TRVL</h1>
       </Link>
       <ul className={click ? 'nav-menu active' : 'nav-menu'}>
         <li>
